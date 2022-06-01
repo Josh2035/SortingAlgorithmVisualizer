e0c895249eb9e31fe9d94889f806e5f63bd7fa6f
Josh2035 <88112012+Josh2035@users.noreply.github.com>
2022-06-01 17:49:05 -0600

    Fixing Code adding Comments

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from tkinter import *
 import tkinter as tk
 import turtle
 import random
-import math
-from time import sleep
+import sys
 from datetime import datetime
 
 
-def Randomize():
-    # list of objects
-    # Randomizer
+def randomize():
+    """Randomizes"""
     random.seed(datetime.now())
     for turt in range(50):
         x = random.randint(0, 49)
         y = random.randint(0, 49)
-        temp = 0;
-        temp2 = 0;
+        temp = 0
+        temp2 = 0
         temp = turtList[x].xcor()
         temp2 = turtList[y].xcor()
         turtList[x].goto(temp2, -145)
@@ -24,32 +21,31 @@ def Randomize():
         turtList[x], turtList[y] = turtList[y], turtList[x]
 
 
-def Insertion():
-    ## Have a time function (THen have a previous Time that adds to list) FOR EXMPLAE ARRAY: INSERTIONO: 14 seconds
+def exitprog():
+    """exit case"""
+    sys.exit()
+
+
+def insertion():
+    """insertion Sort"""
     #
     # Traverse through 1 to len(arr)
     for i in range(1, len(turtList)):
-        turtList[i].color("red")
         key = sortArr[i]
 
         j = i - 1
         while j >= 0 and key < sortArr[j]:
-            turtList[j + 1].color("green")
-            turtList[j].color("green")
             temp = turtList[j + 1].xcor()
             temp2 = turtList[j].xcor()
             turtList[j + 1].goto(temp2, -145)
             turtList[j].goto(temp, -145)
-            turtList[j + 1].color("black")
-            turtList[j].color("black")
             sortArr[j], sortArr[j + 1] = sortArr[j + 1], sortArr[j]
             turtList[j], turtList[j + 1] = turtList[j + 1], turtList[j]
             j -= 1
 
-        turtList[i].color("black")
 
-
-def Bubble():
+def bubble():
+    """Bubble Sort"""
     n = len(sortArr)
     # Traverse through all array elements
     for i in range(n):
@@ -59,31 +55,24 @@ def Bubble():
             # Swap if the element found is greater
             # than the next element
             if sortArr[j] > sortArr[j + 1]:
-                turtList[j + 1].color("green")
-                turtList[j].color("green")
                 # sleep(0.2)
                 temp = turtList[j + 1].xcor()
                 temp2 = turtList[j].xcor()
                 turtList[j + 1].goto(temp2, -145)
                 turtList[j].goto(temp, -145)
-                turtList[j + 1].color("black")
-                turtList[j].color("black")
                 sortArr[j], sortArr[j + 1] = sortArr[j + 1], sortArr[j]
                 turtList[j], turtList[j + 1] = turtList[j + 1], turtList[j]
 
 
-def Selection():
+def selection():
+    """Selection"""
     for i in range(len(sortArr)):
         # Find the minimum element in remaining
         # unsorted array
         min_idx = i
         for j in range(i + 1, len(sortArr)):
-            turtList[j].color("red")
             if sortArr[min_idx] > sortArr[j]:
-                turtList[min_idx].color("black")
                 min_idx = j
-                turtList[min_idx].color("green")
-            turtList[j].color("black")
         # Swap the found minimum element with
         # the first element
         temp = turtList[i].xcor()
@@ -94,15 +83,15 @@ def Selection():
         turtList[i], turtList[min_idx] = turtList[min_idx], turtList[i]
 
 
-def Check():
-    Mode = v.get()
-    if Mode == 0:
-        Insertion()
-    elif Mode == 1:
-        Bubble()
-    elif Mode == 2:
-        Selection()
-
+def check():
+    """checks mode"""
+    mode = v.get()
+    if mode == 0:
+        insertion()
+    elif mode == 1:
+        bubble()
+    elif mode == 2:
+        selection()
 
 # Initial root
 root = tk.Tk()
@@ -114,30 +103,17 @@ v.set(0)
 # List
 l1 = tk.Label(root, text="The Sorting Visualizer")
 l1.grid(row=11, column=5)
-l2 = tk.Label(root, text="Sort Speed")
-l2.grid(row=1, column=11)
 l3 = tk.Label(root, text="Sort Type")
 l3.grid(row=3, column=11)
-l4 = tk.Label(root, text="Time Elapsed")
-l4.grid(row=7, column=11)
-
-# PUT Time Here
-l5 = tk.Label(root, text="0.00")
-l5.grid(row=8, column=11)
 
 # Button
-b1 = tk.Button(root, text="Randomize", command=Randomize)
+b1 = tk.Button(root, text="randomize", command=randomize)
 b1.grid(row=12, column=5)
-b2 = tk.Button(root, text="Go", command=Check)
-b2.grid(row=10, column=11)
-# Entry
-# e1=tk.Entry(root, width=35)
-# e1.grid(row=12,column=1)
+b2 = tk.Button(root, text="Go", command=check)
+b2.grid(row=7, column=11)
+b3 = tk.Button(root, text="Exit", command=exitprog)
+b3.grid(row=12, column=11)
 
-# Step Slider
-s1 = tk.Scale(root, from_=0, to=10, orient=tk.HORIZONTAL,
-              width=10, length=80)
-s1.grid(row=2, column=11, sticky=tk.W)
 # Type of Operation
 o1 = tk.Radiobutton(root, text="  Insertion", variable=v, value="0")
 o2 = tk.Radiobutton(root, text="  Bubble", variable=v, value="1")
@@ -145,16 +121,19 @@ o3 = tk.Radiobutton(root, text="  Selection", variable=v, value="2")
 o1.grid(row=4, column=11, sticky=tk.W)
 o2.grid(row=5, column=11, sticky=tk.W)
 o3.grid(row=6, column=11, sticky=tk.W)
+
 # Turtle
 canvas = tk.Canvas(master=root, width=300, height=300)
 
 canvas.grid(row=1, column=1, columnspan=10, rowspan=10)
 turtList = [turtle.RawTurtle(canvas) for i in range(50)]
-sortArr = []
 for turt in range(50):
-    # turtList[turt].turtle.RawTurtle(canvas)
+    turtList[turt].color("white")
+
+sortArr = []
+
+for turt in range(50):
     sortArr.append(turt)
-    turtList[turt].color("black")
     turtList[turt].ht()
     turtList[turt].speed(0)
     turtList[turt].pu()
@@ -163,9 +142,8 @@ for turt in range(50):
     turtList[turt].turtlesize(stretch_wid=(1 / 5), stretch_len=((int(turt) + 1) * .5), outline=None)
     turtList[turt].goto((turt * 6 - 145 / .95), (-145))
     turtList[turt].st()
+    turtList[turt].color("black")
 # Turtle Creation
-# ADD FOR LOOP
-
 # Mainloop
+
 root.mainloop()
-# nice
